10547cc52623e8b04b964847071f065c8aa6ff1e
isotop786 <marufislam77@gmail.com>
2024-04-27 18:26:23 +0600

    express configured with CORS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("welcome")
+import express, {Request,Response} from "express"
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors({
+    origin:['http://localhost:4200']
+}))
+
+app.get('/',(req:Request, res: Response)=>{
+    res.send("Hello There!")
+})
+
+app.listen(8000,()=>{
+    console.log(`listing to port 8000`)
+})
